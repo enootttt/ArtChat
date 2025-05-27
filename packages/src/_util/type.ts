308b8037554e22a0342496c4552c7fd251c47e67
lihuifeng <lihuifeng@f-cloud.com.cn>
2025-05-27 11:49:47 +0800

    feat(_util): 增加GenericComponentExports用来获取泛型组件类型

--- packages/src/_util/type.ts
+++ packages/src/_util/type.ts
@@ -1,1 +1,8 @@
+import type { Component } from 'vue'
+import type { ComponentExposed, ComponentProps, ComponentType } from 'vue-component-type-helpers'
+/** 泛型组件出口类型 */
+export type GenericComponentExports<D extends Component> = ComponentType<D> &
+  ComponentExposed<D> &
+  ComponentProps<D>
+
 export type AnyObject = Record<PropertyKey, any>
