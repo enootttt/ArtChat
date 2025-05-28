d449059724dd8873595289c93f8a5a83d4f11a5c
lihuifeng <lihuifeng@f-cloud.com.cn>
2025-05-28 13:16:15 +0800

    build(stylelint): 在提交代码时不进行自动化格式scss文件操作

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 export default {
   '*.ts?(x)': ['eslint --fix'],
-  '*.{scss,less,css,html}': ['stylelint --fix'],
+  '*.{scss,less,css,html}': ['stylelint'],
   '*.{ts?(x),cjs,scss,less,css,html,json,md}': ['prettier --write'],
 }
