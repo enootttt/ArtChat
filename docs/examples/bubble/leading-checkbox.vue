944758a10abe77c155a339fdb3faae39ca2d01b3
lihuifeng <lihuifeng@f-cloud.com.cn>
2025-06-05 14:44:29 +0800

    docs(Bubble): 优化选中消息文档展现方式

--- docs/examples/bubble/leading-checkbox.vue
+++ docs/examples/bubble/leading-checkbox.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import type { BubbleListProps } from '@artmate/chat'
+import type { CheckboxValueType } from 'element-plus'
 import { BubbleList } from '@artmate/chat'
-import { ElCheckbox, ElSpace, ElTag } from 'element-plus'
+import { ElCheckbox, ElCheckboxGroup, ElSpace, ElTag } from 'element-plus'
 import { computed, ref } from 'vue'
 
 const BubbleListRef = ref<InstanceType<typeof BubbleList>>()
@@ -20,15 +21,13 @@ const rolesAsObject: BubbleListProps['roles'] = {
 
 const count = ref<number>(10)
 
-// const useRolesAsFunction = ref(false);
-
 const items = computed<BubbleListProps['items']>(() =>
   Array.from({ length: count.value }).map((_, i) => {
     const isAI = !!(i % 2)
     const content = isAI ? 'Mock AI content. '.repeat(20) : 'Mock user content.'
 
     return {
-      key: `${i}`,
+      key: `${i + 1}`,
       role: isAI ? 'ai' : 'user',
       // 保证只有最新的一条回话会打印输出
       typing: isAI && i === count.value - 1 && { step: 5, interval: 20 },
@@ -37,35 +36,52 @@ const items = computed<BubbleListProps['items']>(() =>
   })
 )
 
-const selectList = ref<Array<string | number | undefined>>([])
+// ------------ 选中数据 ------------
+const selectList = ref<Array<string>>([])
 
-function selectCheckbox(key: string | number | undefined) {
-  console.log(key)
-  if (selectList.value.includes(key)) {
-    selectList.value = selectList.value.filter((item) => item !== key)
-  } else {
-    selectList.value.push(key)
-  }
+// ------------ 全选逻辑 ------------
+const checkAll = ref(false)
+const isIndeterminate = ref(false)
+
+function handleCheckAllChange(val: CheckboxValueType) {
+  selectList.value = val ? items.value.map((item) => item.key as string) : []
+  isIndeterminate.value = false
+}
+
+function handleCheckedCitiesChange(value: CheckboxValueType[]) {
+  const checkedCount = value.length
+  checkAll.value = checkedCount === items.value.length
+  isIndeterminate.value = checkedCount > 0 && checkedCount < items.value.length
 }
 </script>
 
 <template>
   <ElSpace direction="vertical" alignment="align-start">
-    <BubbleList
-      ref="BubbleListRef"
-      :items="items"
-      :roles="rolesAsObject"
-      auto-scroll
-      :style="{ maxHeight: '300px' }"
-    >
-      <template #leading="{ info }">
-        <ElCheckbox :checked="selectList.includes(info.key)" @click="selectCheckbox(info.key)" />
-      </template>
-    </BubbleList>
+    <div>
+      <ElCheckbox
+        v-model="checkAll"
+        :indeterminate="isIndeterminate"
+        @change="handleCheckAllChange"
+      >
+        全选
+      </ElCheckbox>
+    </div>
+    <ElCheckboxGroup v-model="selectList" @change="handleCheckedCitiesChange">
+      <BubbleList
+        ref="BubbleListRef"
+        :items="items"
+        :roles="rolesAsObject"
+        auto-scroll
+        :style="{ maxHeight: '300px' }"
+      >
+        <template #leading="{ info }">
+          <ElCheckbox :value="info.key" />
+        </template>
+      </BubbleList>
+    </ElCheckboxGroup>
     <div style="display: flex; flex-wrap: wrap; gap: 4px">
-      已选中：<ElTag v-for="key in selectList" :key="key">
-        key为 {{ key }} 的消息
-      </ElTag>
+      已选中：
+      <ElTag v-for="key in selectList" :key="key">key为 {{ key }} 的消息</ElTag>
     </div>
   </ElSpace>
 </template>
