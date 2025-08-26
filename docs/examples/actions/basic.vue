45a343373e0329155271a9058690e6d27f840f6b
lihuifeng <lihuifeng@f-cloud.com.cn>
2025-08-26 18:14:16 +0800

    docs(actions): 新增actions组件文档

---
+++ docs/examples/actions/basic.vue
@@ -0,0 +1,32 @@
+<script setup lang='ts'>
+import type { ActionsProps } from '@artmate/chat'
+import { Actions } from '@artmate/chat'
+
+import { ElMessage } from 'element-plus'
+import { RefreshRight, DocumentCopy } from '@element-plus/icons-vue'
+
+const actionItems: ActionsProps['items'] = [
+  {
+    key: 'retry',
+    label: 'Retry',
+    icon: RefreshRight
+  },
+  {
+    key: 'copy',
+    label: 'Copy',
+    icon: DocumentCopy
+  },
+]
+
+const onClick = ({ keyPath }: { keyPath: string[]  }) => {
+  ElMessage.success(`you clicked ${keyPath.join(',')}`)
+}
+</script>
+
+<template>
+  <div>
+    <Actions :items="actionItems" @click="onClick" />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ docs/examples/actions/more.vue
@@ -0,0 +1,55 @@
+<script setup lang='ts'>
+import type { ActionsProps } from '@artmate/chat'
+import { Actions } from '@artmate/chat'
+
+import { ElMessage } from 'element-plus'
+import { RefreshRight, DocumentCopy, Delete, MoreFilled, Share } from '@element-plus/icons-vue'
+
+const actionItems: ActionsProps['items'] = [
+  {
+    key: 'retry',
+    label: 'Retry',
+    icon: RefreshRight
+  },
+  {
+    key: 'copy',
+    label: 'Copy',
+    icon: DocumentCopy
+  },
+  {
+    key: 'more',
+    label: 'more',
+    icon: MoreFilled,
+    children: [
+      {
+        key: 'share',
+        label: 'Share',
+        icon: Share
+      },
+      { key: 'import', label: 'Import' },
+      {
+        key: 'delete',
+        label: 'Delete',
+        disabled: true
+      },
+    ],
+  },
+  {
+    key: 'clear',
+    label: 'Clear',
+    icon: Delete
+  },
+]
+
+const onClick = ({ keyPath }: { keyPath: string[]  }) => {
+  ElMessage.success(`you clicked ${keyPath.join(',')}`)
+}
+</script>
+
+<template>
+  <div>
+    <Actions :items="actionItems" @click="onClick" />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
